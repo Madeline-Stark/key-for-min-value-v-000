dbfadaf13cf115521e4a66c6e2cac396658695fc
Madeline Stark <madeline.stark1@gmail.com>
2018-03-29 16:56:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  a = name_hash.values.map
   name_hash.collect do |key, value|
-    if value == name_hash.values.min
+    if value == a.values.min
     min = key
   end
   end
